f8c8361ab37e1eb8d5ed577bb00e52d5dd932026
Dhruv Singh K <dhruvsinghk@Dhruvs-MacBook-Pro.local>
2022-06-21 12:51:21 +0530

    Changes by Dhruv

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class TestJava {
   public static void main(String args[]){
-    System.out.println("First file on GIT hub");
+    System.out.println("Second file change on GIT hub by Dhruv");
     System.out.println("Second change");
   }
 }
