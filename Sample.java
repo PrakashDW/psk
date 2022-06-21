c5a7ebfedb77725a67fbbbaf48f11cf82166f109
Prakash Kushwah <prakash@Prakashs-MacBook-Air.local>
2022-06-21 12:48:33 +0530

    4th chnage for branch exrmnt

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class TestJava {
   public static void main(String args[]){
     System.out.println("First file on GIT hub");
-    System.out.println("Second change: branch psk2");
+    System.out.println("Second change: branch psk2: one more chnage for branch to brnch push");
   }
 }
